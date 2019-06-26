9aca9b7b4b33c8e1aa13e6326ed45898ff396156
Joachim Werner <joe@suse.com>
2019-06-26 11:12:18 +0200

    Cleanup and CLI example for 'set_property'.

--- _modules/redfish.py
+++ _modules/redfish.py
@@ -21,18 +21,18 @@ def system_property(property):
     else:
         return False
 
-#def setting(property, value):
-    #payload = {property: value}
-    #headers = {'content-type': 'application/json'}
-    #response = requests.patch(
-        #'https://%s/redfish/v1/Chassis/System.Embedded.1/Settings' % __opts__['redfish']['host'],
-        #data=json.dumps(payload),
-        #headers=headers,
-        #verify=False,
-        #auth=(__opts__['redfish']['user'], __opts__['redfish']['pwd']))
-    #return response.__dict__
-
 def set_property(property, value):
+    '''
+    This is currently only known to work for setting the IndicatorLED like this:
+    
+    .. code-block:: bash
+    
+        salt-call redfish.set_property IndicatorLED "Blinking"
+        salt-call redfish.set_property IndicatorLED "'Off'"
+
+    The double quotes are required for the "Off" property because otherwise it will be interpreted as a Python "False"!
+    '''
+    
     payload = {property: value}
     headers = {'content-type': 'application/json'}
     response = requests.patch(
